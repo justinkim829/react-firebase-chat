0dd480b7875294ded5583c035a9cfc98bbceb73c
jinsek <jinsek@cs.washington.edu>
2024-06-11 11:56:44 -0700

    changes

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,39 @@
 import './login.css';
-import React from 'react';
+import React, {useState} from 'react';
 
 function Login() {
+
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: ""
+  });
+
   return (
     <div className='login'>
+      <div className="item">
+        <h2>Welcome Back</h2>
+        <form>
+          <input type="text" placeholder='Email' name='email'/>
+          <input type="password" placeholder='Password' name='password'/>
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="seperator">
 
+      </div>
+      <div className="item">
+        <h2>Create An Account</h2>
+        <form>
+          <label htmlFor="file">Upload an Image
+            <img src="" alt="" />
+          </label>
+          <input type="file" id="file" style={{display: "none"}}/>
+          <input type="text" placeholder='Username' name='username'/>
+          <input type="text" placeholder='Email' name='email'/>
+          <input type="password" placeholder='Password' name='password'/>
+          <button>Sign In</button>
+        </form>
+      </div>
     </div>
   );
 }
