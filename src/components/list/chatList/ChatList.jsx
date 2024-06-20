8be9d581837c686a66b8f3bbfbdf77d3528b4c5f
jinsek <jinsek@cs.washington.edu>
2024-06-19 20:16:16 -0700

    changes from desktop

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,11 +1,40 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./chatList.css";
 import AddUser from './addUser/AddUser';
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { useUserStore } from '../../../lib/userStore';
+import { db } from '../../../lib/firebase';
 
 function ChatList() {
 
   // is add mode clicked next to search input
   const [addMode, setAddMode] = useState(false);
+  const [chats, setChats] = useState([]);
+
+  const {currentUser} = useUserStore();
+
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "userchats", currentUser.id), async (res) => {
+      const items = res.data().chats;
+
+      const promises = items.map(async (item) => {
+        const userDocRef = doc(db, "users", item.receiverId);
+        const userDocSnap = await getDoc(userDocRef);
+
+        const user = userDocSnap.data();
+
+        return {...item, user};
+      });
+
+      const chatData = await Promise.all(promises);
+      setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+    });
+
+    return () => {
+      unsub();
+    }
+  }, [currentUser.id]);
 
   return (
     <div className='chat-list'>
@@ -17,28 +46,23 @@ function ChatList() {
         <img src={addMode ? "minus.png" : "plus.png"} alt="plus"
              className='add'onClick={() => setAddMode(prev => !prev)}/>
       </div>
-      <div className="item">
+      {chats.map((chat) => {
+        <div className="item" key={chat.chatId}>
           <img src="avatar.png" alt="avatar" />
           <div className="texts">
             <span>Jayden</span>
-            <p>Hello</p>
+            <p>{chat.lastMessage}</p>
           </div>
         </div>
-        <div className="item">
-          <img src="avatar.png" alt="avatar" />
-          <div className="texts">
-            <span>Jayden</span>
-            <p>Hello</p>
-          </div>
+      })}
+      {/* <div className="item">
+        <img src="avatar.png" alt="avatar" />
+        <div className="texts">
+          <span>Jayden</span>
+          <p>Hello</p>
         </div>
-        <div className="item">
-          <img src="avatar.png" alt="avatar" />
-          <div className="texts">
-            <span>Jayden</span>
-            <p>Hello</p>
-          </div>
-        </div>
-        {addMode && <AddUser/>}
+      </div> */}
+      {addMode && <AddUser/>}
     </div>
   )
 }
--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,20 +1,54 @@
-import React from 'react';
+import { useState } from 'react';
 import "./add-user.css";
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../../../../lib/firebase';
 
 function AddUser() {
+
+  const [user, setUser] = useState(null);
+
+  // No problems in handleSearch
+  async function handleSearch(evt) {
+    evt.preventDefault();
+    const formData = new FormData(evt.target);
+    const username = formData.get("username");
+
+    try {
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("username", "==", username));
+      const querySnapShot = await getDocs(q);
+
+      if (!querySnapShot.empty) {
+        setUser(querySnapShot.docs[0].data());
+      }
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  async function handleAdd() {
+    const chatRef = collection(db, "chats");
+
+    try {
+      console.log(1);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className='add-user'>
-      <form>
+      <form onSubmit={handleSearch}>
         <input type="text" placeholder='Username' name='username'/>
         <button>Search</button>
       </form>
-      <div className="user">
+      {user && <div className="user">
         <div className="detail">
-          <img src="./avatar.png" alt="avatar" />
-          <span>Jane</span>
+          <img src={user.avatar || "./avatar.png"} alt="avatar" />
+          <span>{user.username}</span>
         </div>
-        <button>Add User</button>
-      </div>
+        <button onClick={handleAdd}>Add User</button>
+      </div>}
     </div>
   )
 }
